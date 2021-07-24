b4c122aa58cec1bd87d8c2ee99acc6148b0d33be
Kiều Nguyễn <beu_csnv@outlook.com>
2021-07-24 09:56:19 +0700

    Add project files.

---
+++ 1_0_EFCORE_DataBaseFirts/AccountService.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using _1_0_EFCORE_DataBaseFirts.Context;
+
+namespace _1_0_EFCORE_DataBaseFirts
+{
+    public class AccountService
+    {
+        private DatabaseContext dbContext;
+        public AccountService()
+        {
+            dbContext = new DatabaseContext();
+            var listACC = dbContext.AccountsAdos.ToList();
+        }
+    }
+}
---
+++ 1_0_EFCORE_DataBaseFirts/Context/DatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using _1_0_EFCORE_DataBaseFirts.Models;
+
+#nullable disable
+
+namespace _1_0_EFCORE_DataBaseFirts.Context
+{
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+        {
+        }
+
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<AccountsAdo> AccountsAdos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-NVB7S6L;Initial Catalog=Kieu_CSharp_3;Persist Security Info=True;User ID=kieu96;Password=123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<AccountsAdo>(entity =>
+            {
+                entity.Property(e => e.Id).HasDefaultValueSql("(newsequentialid())");
+
+                entity.Property(e => e.Acc).IsUnicode(false);
+
+                entity.Property(e => e.Pass).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
